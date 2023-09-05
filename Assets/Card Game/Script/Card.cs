3ef13d56149c5e28b88c55fe9ebf7c726b924eef
Nue-X <1962785977@qq.com>
2023-09-05 15:15:02 +0800

    增加卡牌以及预制体

---
+++ Assets/Card Game/Script/Card.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniGames.CardGame
+{
+    [Serializable]
+    public class Card
+    {
+        public int id;
+        public string name;
+        public int cost;
+        public int atk;
+        [TextArea] public string description;
+
+        public Card(int id, string name, int cost, int atk, string description)
+        {
+            this.id = id;
+            this.name = name;
+            this.cost = cost;
+            this.atk = atk;
+            this.description = description;
+        }
+    }
+}
