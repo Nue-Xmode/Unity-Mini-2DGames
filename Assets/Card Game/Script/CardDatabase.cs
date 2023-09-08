23adba681ce3ff52fa6d6f2ca6f696f5040d59dd
Nue-X <1962785977@qq.com>
2023-09-08 15:45:12 +0800

    增加卡牌图片读取功能

--- Assets/Card Game/Script/CardDatabase.cs
+++ Assets/Card Game/Script/CardDatabase.cs
@@ -1,17 +1,32 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Utilitiees;
 
 namespace MiniGames.CardGame
 {
-    public class CardDatabase : MonoBehaviour
+    public class CardDatabase : Singleton<CardDatabase>
     {
         public static List<Card> CardList = new List<Card>();
+        public List<Sprite> spriteList = new List<Sprite>();
 
-        private void Awake()
+        public override void Awake()
         {
+            base.Awake();
             CardList.Add(new Card(0, "Human", 1, 1, "a human"));
             CardList.Add(new Card(1, "Dragon", 7, 5, "a dragon"));
             CardList.Add(new Card(2, "Elf", 3, 2, "a elf"));
+        }
+        
+        /// <summary>
+        /// 获取特定卡牌的图片
+        /// </summary>
+        /// <returns></returns>
+        public Sprite GetCardSprite(int displayId)
+        {
+            if (displayId < CardList.Count)
+                return spriteList.Find(s => s.name == CardList[displayId].name);
+
+            return null;
         }
     }
 }
