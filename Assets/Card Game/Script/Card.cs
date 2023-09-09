87b62fc8fafeeff90099c4652fde93f69241416e
Nue-X <1962785977@qq.com>
2023-09-09 15:40:44 +0800

    增加简易牌堆功能

--- Assets/Card Game/Script/Card.cs
+++ Assets/Card Game/Script/Card.cs
@@ -12,6 +12,7 @@ namespace MiniGames.CardGame
         public int atk;
         [TextArea] public string description;
 
+        public Card () {}
         public Card(int id, string name, int cost, int atk, string description)
         {
             this.id = id;
@@ -19,6 +20,24 @@ namespace MiniGames.CardGame
             this.cost = cost;
             this.atk = atk;
             this.description = description;
+        }
+
+        /// <summary>
+        /// 获取卡牌数据的副本
+        /// </summary>
+        /// <returns></returns>
+        public Card Copy()
+        {
+            Card newCard = new Card()
+            {
+                id = this.id,
+                name = this.name,
+                cost = this.cost,
+                atk = this.atk,
+                description = this.description
+            };
+
+            return newCard;
         }
     }
 }
